514253e22b654b5f3fae45bcac7f32adf83cf727
localvos <dev@dvos.me>
2025-12-01 21:48:28 +0100

    chore: cleanup logging output of answer

--- Sources/main.swift
+++ Sources/main.swift
@@ -1,11 +1,19 @@
 import Foundation
 
-func readFile() -> String {
-    let fileURL = URL(fileURLWithPath: "resources/input-1")
+func readFile(filePath: String) -> String {
+    let fileURL = URL(fileURLWithPath: filePath)
     let contents = try? String(contentsOf: fileURL, encoding: .utf8)
     return contents ?? "Failed to open file"
 }
 
-let fileString = readFile()
-print("Part One: \(part1(fileString: fileString))")
-print("Part Two: \(part2(fileString: fileString))")
+print(
+    "================================" +
+    "\n" +
+    "Day 1: \n" +
+    "TotalTime spent: 45 min \n" +
+    "================================ \n"
+)
+print("Part One: \(part1(fileString: readFile(filePath: "resources/input-1")))")
+print("Part Two: \(part2(fileString: readFile(filePath: "resources/input-1")))")
+
+
